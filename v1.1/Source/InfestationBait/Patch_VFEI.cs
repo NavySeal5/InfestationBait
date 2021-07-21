dffde3183f5518b59f76960adc100a2620482168
NavySeal5 <tobi1.w@gmail.com>
2021-07-21 11:22:02 +0200

    Add Support for 1.3

---
+++ v1.1/Source/InfestationBait/Patch_VFEI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+
+namespace InfestationBait
+{
+
+	//[HarmonyPatch()]
+	//[HarmonyPatch("TryFindCell")]
+	public class Patch_VFEI
+	{
+		public static void Postfix(bool __result, ref IntVec3 cell, Map map)
+		{
+			if (__result)
+			{
+				bool isBaitApplied = Verse.Rand.Chance((float)InfestationBaitSettings.baitChance / 100f);
+				bool mapHasBait = InfestationBaitSettings.mapDict.ContainsKey(map);
+
+				if (isBaitApplied && mapHasBait)
+				{
+					var building = InfestationBaitSettings.mapDict[map];
+					cell = building.Position;
+
+				}
+			}
+		}
+	}
+}
---
+++ v1.3/Source/InfestationBait/Patch_LoadGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using Verse;
+
+namespace InfestationBait{
+
+	[HarmonyPatch(typeof(Game))]
+	[HarmonyPatch("LoadGame")]
+	internal static class Patch_LoadGame{
+		
+		private static bool Prefix(){
+			InfestationBaitSettings.mapDict=new Dictionary<Map,Thing>();
+			return true;
+		}
+	}
+}
